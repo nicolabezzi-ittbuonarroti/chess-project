8ebdb2b8e87216004cdd8960aac9cd644deee651
Delu <davide.delucia@buonarroti.tn.it>
2023-02-07 17:00:28 +0100

    fixed typing error

--- ChessProject/src/com/company/ChessPiece.java
+++ ChessProject/src/com/company/ChessPiece.java
@@ -1,4 +1,28 @@
 package com.company;
+import java.util.ArrayList;
 
-public class ChessPiece {
+public abstract class ChessPiece{
+    private Position pos;
+
+    public ChessPiece(Position pos){
+        this.pos = pos;
+    }
+
+    public Position getPosition(){
+        return this.pos;
+    }
+
+    public boolean moveTo(Position pos){
+        boolean moved=false;
+        if(this.validMoves().contains(pos)){
+            this.pos = pos;
+            moved = true;
+        }
+        return moved;
+    }
+
+    public ArrayList<Position> validMoves(){
+
+    }
+
 }
